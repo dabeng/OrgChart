da10bc543b879ae6614fd371e7d28821b9623276
dabeng <zhexiany@gmail.com>
2016-02-18 21:18:19 +0800

    clean up styles

--- ondemand-loading-data/scripts.js
+++ ondemand-loading-data/scripts.js
@@ -4,6 +4,58 @@
 
   $(function() {
 
+    $.mockjax({
+      url: '/orgchart/children/3/',
+      contentType: 'application/json',
+      responseTime: 1000,
+      responseText: { 'children': [
+        { 'id': '4', 'name': 'Pang Pang', 'title': 'engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 1 }},
+        { 'id': '5', 'name': 'Xiang Xiang', 'title': 'UE engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 1 }}
+      ]}
+    });
+
+    $.mockjax({
+      url: '/orgchart/parent/1/',
+      contentType: 'application/json',
+      responseTime: 1000,
+      responseText: { 'id': '6','name': 'Lao Lao', 'title': 'general manager', 'relationship': { 'children_num': 8 }}
+    });
+
+    $.mockjax({
+      url: '/orgchart/siblings/1/',
+      contentType: 'application/json',
+      responseTime: 1000,
+      responseText: { 'siblings': [
+        { 'id': '7','name': 'Bo Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '8', 'name': 'Yu Jie', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '9', 'name': 'Yu Li', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '10', 'name': 'Hong Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '11', 'name': 'Yu Wei', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '12', 'name': 'Chun Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '13', 'name': 'Yu Tie', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }}
+      ]}
+    });
+
+    $.mockjax({
+      url: '/orgchart/families/1/',
+      contentType: 'application/json',
+      responseTime: 1000,
+      responseText: {
+        'id': '6',
+        'name': 'Lao Lao',
+        'title': 'general manager',
+        'relationship': { 'children_num': 8 },
+        'children': [
+        { 'id': '7','name': 'Bo Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '8', 'name': 'Yu Jie', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '9', 'name': 'Yu Li', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '10', 'name': 'Hong Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '11', 'name': 'Yu Wei', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '12', 'name': 'Chun Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'id': '13', 'name': 'Yu Tie', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }}
+      ]}
+    });
+
     var datascource = {
       'id': '1',
       'name': 'Su Miao',
@@ -22,79 +74,25 @@
       'families': '/orgchart/families/' 
     };
 
-    $.mockjax({
-      url: '/orgchart/children/3/',
-      contentType: 'application/json',
-      responseTime: 2000,
-      responseText: { 'children': [
-        { 'id': '4', 'name': 'Pang Pang', 'title': 'engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 1 }},
-        { 'id': '5', 'name': 'Xiang Xiang', 'title': 'UE engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 1 }}
-      ]}
-    });
-
-    $.mockjax({
-      url: '/orgchart/parent/1/',
-      contentType: 'application/json',
-      responseTime: 2000,
-      responseText: { 'id': '6','name': 'Lao Lao', 'title': 'general manager', 'relationship': { 'children_num': 8 }}
-    });
-
-    $.mockjax({
-      url: '/orgchart/siblings/1/',
-      contentType: 'application/json',
-      responseTime: 2000,
-      responseText: { 'siblings': [
-        { 'id': '7','name': 'Bo Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '8', 'name': 'Yu Jie', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '9', 'name': 'Yu Li', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '10', 'name': 'Hong Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '11', 'name': 'Yu Wei', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '12', 'name': 'Chun Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '13', 'name': 'Yu Tie', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }}
-      ]}
-    });
-
-    $.mockjax({
-      url: '/orgchart/families/1/',
-      contentType: 'application/json',
-      responseTime: 2000,
-      responseText: {
-        'id': '6',
-        'name': 'Lao Lao',
-        'title': 'general manager',
-        'relationship': { 'children_num': 8 },
-        'children': [
-        { 'id': '7','name': 'Bo Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '8', 'name': 'Yu Jie', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '9', 'name': 'Yu Li', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '10', 'name': 'Hong Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '11', 'name': 'Yu Wei', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '12', 'name': 'Chun Miao', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
-        { 'id': '13', 'name': 'Yu Tie', 'title': 'department engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }}
-      ]}
-    });
-
- $('#chart-container').orgchart({
-    'data' : datascource,
-    'ajaxURL': ajaxURLs,
-    // 'chartClass': 'oc-employee',
-    'nodeTitle': ['name'],
-    'nodeContent': ['title'],
-    'nodeID': 'id',
-    'create': function() {
-      var chartContainer = $('#chart-container');
-      var chart = chartContainer.find('.orgchart');
-      var containerWidth = chartContainer.width();
-      if (chartContainer[0].scrollWidth > containerWidth) {
-        chartContainer.scrollLeft(chartContainer[0].scrollWidth/2 - containerWidth/2);
-      } else {
-        chart.css('left', containerWidth/2 - chart.outerWidth(true)/2);
+    $('#chart-container').orgchart({
+      'data' : datascource,
+      'ajaxURL': ajaxURLs,
+      // 'chartClass': 'oc-employee',
+      'nodeTitle': ['name'],
+      'nodeContent': ['title'],
+      'nodeID': 'id',
+      'create': function() {
+        var chartContainer = $('#chart-container');
+        var chart = chartContainer.find('.orgchart');
+        var containerWidth = chartContainer.width();
+        if (chartContainer[0].scrollWidth > containerWidth) {
+          chartContainer.scrollLeft(chartContainer[0].scrollWidth/2 - containerWidth/2);
+        } else {
+          chart.css('left', containerWidth/2 - chart.outerWidth(true)/2);
+        }
+        chart.css('top',30);
       }
-      chart.css('top',30);
-    }
-  });
-
- mockJqueryAjax();
+    });
 
   });
 
