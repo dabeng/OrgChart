ccc95b6b13202e9d2ccd31b55c63f3a78c2c42a2
dabeng <zhexiany@gmail.com>
2016-02-23 09:10:56 +0800

    clean up code

--- option-createNode/scripts.js
+++ option-createNode/scripts.js
@@ -31,70 +31,23 @@
       ]
     };
 
- $('#chart-container').orgchart({
-    'data' : datascource,
-    'depth': 2,
-    'nodeTitle': 'name',
-    'nodeContent': 'title',
-    'nodeID': 'id',
-    'createNode': function($node, data) {
-      // attach relatedId used for switch between employee/group charts
-      // $node.data('relatedId', (data.group) ? data.group.id: '');
-      // $node.children('.title').attr('title', data.realname);
-
-      // add strikethrough to terminated employees
-      // if (data.is_terminated) {
-      //   $node.find('.title').addClass('inactive');
-      // }
-
-      // append 2nd menu including detailed info of node
-      var nodePrompt = $('<i>', {
-        'class': 'fa fa-info-circle second-menu-icon',
-        click: function() {
-          $(this).siblings('.second-menu').toggle();
-        }
-      });
-      // var secondMenu =
-      //   '<div class="second-menu">' +
-      //     '<div class="popover right oc-popupmenu">' +
-      //       '<div class="arrow"></div>' +
-      //       '<h2 class="popover-title"></h2>' +
-      //       '<div class="popover-content">' +
-      //       '<dl class="oc-popupmenu-info"></dl>' +
-      //       '</div>' +
-      //    '</div>' +
-      //     '</div>';
-var secondMenu = '<div class="second-menu"><img class="avatar" src="../img/avatar/' + data.id + '.jpg"></div>';
-      $node.append(nodePrompt).append(secondMenu);
-
-      // contruct the url of realname
-      // var url = window.location.origin;
-      // var urlConstructContent = {
-      //   'text': data.realname,
-      //   'href': url + '/employee/' + data.uid,
-      //   'click': function(event) {
-      //       event.stopPropagation();
-      //   }
-      // };
-      // var urlElement = $('<a>', urlConstructContent);
-      // if (data.is_terminated) {
-      //   urlElement.addClass('inactive');
-      // }
-
-      // append detailed info to 2nd menu
-      // $node.find('.popover-title').append(urlElement);
-      // var extraInfo = $node.find('.oc-popupmenu-info');
-      // extraInfo.append($('<dt><div class="user-img" style="background-image: url(\'avatar.jpg\')">'))
-      //   .append($('<dt>kerberos</dt><dd>' + data.uid + '</dd>'))
-      //   .append($('<dt>title</dt><dd>' + data.title + '</dd>'))
-      //   .append($('<dt>group</dt><dd>' + ((data.group) ? data.group.name : '') + '</dd>'))
-      //   .append($('<dt class="email">email</dt><dd>'));
-      // var emailList = extraInfo.find('dt.email').next('dd');
-      // data.email.forEach(function(mail) {
-      //   emailList.append($('<a href="mailto:' + mail + '">' + mail + '</a><br/>'));
-      // });
-    }
-  });
+    $('#chart-container').orgchart({
+      'data' : datascource,
+      'depth': 2,
+      'nodeTitle': 'name',
+      'nodeContent': 'title',
+      'nodeID': 'id',
+      'createNode': function($node, data) {
+        var nodePrompt = $('<i>', {
+          'class': 'fa fa-info-circle second-menu-icon',
+          click: function() {
+            $(this).siblings('.second-menu').toggle();
+          }
+        });
+        var secondMenu = '<div class="second-menu"><img class="avatar" src="../img/avatar/' + data.id + '.jpg"></div>';
+        $node.append(nodePrompt).append(secondMenu);
+      }
+    });
 
   });
 
