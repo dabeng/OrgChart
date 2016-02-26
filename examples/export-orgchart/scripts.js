ca59d54210fe6a8ac03b240b08666a3ed724e1a7
dabeng <zhexiany@gmail.com>
2016-02-26 10:56:37 +0800

    update file structure

---
+++ examples/export-orgchart/scripts.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function($){
+
+  $(function() {
+
+    var datascource = {
+      'name': 'Lao Lao',
+      'title': 'general manager',
+      'relationship': { 'children_num': 8 },
+      'children': [
+        { 'name': 'Bo Miao', 'title': 'department manager', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'name': 'Su Miao', 'title': 'department manager', 'relationship': { 'children_num': 2, 'parent_num': 1,'sibling_num': 7 },
+          'children': [
+            { 'name': 'Tie Hua', 'title': 'senior engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 1 }},
+            { 'name': 'Hei Hei', 'title': 'senior engineer', 'relationship': { 'children_num': 2, 'parent_num': 1,'sibling_num': 1 },
+              'children': [
+                { 'name': 'Pang Pang', 'title': 'engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 1 }},
+                { 'name': 'Xiang Xiang', 'title': 'UE engineer', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 1 }}
+              ]
+            }
+          ]
+        },
+        { 'name': 'Yu Jie', 'title': 'department manager', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'name': 'Yu Li', 'title': 'department manager', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'name': 'Hong Miao', 'title': 'department manager', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'name': 'Yu Wei', 'title': 'department manager', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'name': 'Chun Miao', 'title': 'department manager', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }},
+        { 'name': 'Yu Tie', 'title': 'department manager', 'relationship': { 'children_num': 0, 'parent_num': 1,'sibling_num': 7 }}
+      ]
+    };
+
+    $('#chart-container').orgchart({
+      'data' : datascource,
+      'depth': 2,
+      'nodeTitle': 'name',
+      'nodeContent': 'title',
+      'exportButton': true,
+      'exportFilename': 'MyOrgChart'
+    });
+
+  });
+
+})(jQuery);
