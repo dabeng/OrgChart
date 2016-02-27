93eb32153d7e3925dec4502807c6fcb8794eac04
dabeng <zhexiany@gmail.com>
2016-02-27 09:02:32 +0800

    upload some cites' positions

--- integrate-map/scripts.js
+++ integrate-map/scripts.js
@@ -19,10 +19,8 @@
         ],
         target: 'pageBody',
         view: new ol.View({
-          center: ol.proj.transform(
-              // [-122.416667, 37.783333], 'EPSG:4326', 'EPSG:3857'),
-            [-87.65, 41.883333], 'EPSG:4326', 'EPSG:3857'),
-          zoom: 10
+          center: ol.proj.transform([-87.6297980, 41.8781140], 'EPSG:4326', 'EPSG:3857'),
+          zoom: 12
         })
       });
 
@@ -32,15 +30,16 @@
       'name': 'Lao Lao',
       'title': 'general manager',
       'relationship': '001',
+      'position': [-87.6297980, 41.8781140],
       'children': [
-        { 'name': 'Bo Miao', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Su Miao', 'title': 'department manager', 'relationship': '111' },
-        { 'name': 'Yu Jie', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Yu Li', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Hong Miao', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Yu Wei', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Chun Miao', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Yu Tie', 'title': 'department manager', 'relationship': '110' }
+        { 'name': 'Bo Miao', 'title': 'department manager', 'relationship': '110', 'position': [-0.1277580, 51.5073510]},
+        { 'name': 'Su Miao', 'title': 'department manager', 'relationship': '110', 'position': [151.2069900, -33.8674870]},
+        { 'name': 'Yu Jie', 'title': 'department manager', 'relationship': '110', 'position': [-43.1728960, -22.9068470]},
+        { 'name': 'Yu Li', 'title': 'department manager', 'relationship': '110', 'position': [37.6173000, 55.7558260]},
+        { 'name': 'Hong Miao', 'title': 'department manager', 'relationship': '110', 'position': [13.4049540, 52.5200070]},
+        { 'name': 'Yu Wei', 'title': 'department manager', 'relationship': '110', 'position': [-74.0059410, 40.7127840]},
+        { 'name': 'Chun Miao', 'title': 'department manager', 'relationship': '110', 'position': [103.8198360, 1.3520830]},
+        { 'name': 'Yu Tie', 'title': 'department manager', 'relationship': '110', 'position': [2.3522220, 48.8566140] }
       ]
     };
 
@@ -50,7 +49,7 @@
       'nodeContent': 'title',
       'createNode': function($node, data) {
         $node.on('click', function() {
-          map.getView().setCenter(ol.proj.transform([-122.416667, 37.783333], 'EPSG:4326', 'EPSG:3857'));
+          map.getView().setCenter(ol.proj.transform(data.position, 'EPSG:4326', 'EPSG:3857'));
         });
       }
     });
