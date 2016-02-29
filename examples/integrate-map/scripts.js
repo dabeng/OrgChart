8a88b2442f71c48455b304ca78a1a5b0034f0a96
dabeng <zhexiany@gmail.com>
2016-02-29 10:21:52 +0800

    clean up code

--- examples/integrate-map/scripts.js
+++ examples/integrate-map/scripts.js
@@ -5,59 +5,69 @@
   $(function() {
 
     var map = new ol.Map({
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.Stamen({
-              layer: 'watercolor'
-            })
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.Stamen({
+            layer: 'watercolor'
           }),
-          new ol.layer.Tile({
-            source: new ol.source.Stamen({
-              layer: 'terrain-labels'
-            })
-          })
-        ],
-        target: 'pageBody',
-        view: new ol.View({
-          center: ol.proj.transform(
-              [-122.416667, 37.783333], 'EPSG:4326', 'EPSG:3857'),
-          zoom: 12
+          preload: 4
+        }),
+        new ol.layer.Tile({
+          source: new ol.source.Stamen({
+            layer: 'terrain-labels'
+          }),
+          preload: 1
         })
-      });
+      ],
+      target: 'pageBody',
+      view: new ol.View({
+        center: ol.proj.transform([-87.6297980, 41.8781140], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 10
+      })
+    });
 
-    $('body').prepend(map.a);
+    $('body').prepend(map.getViewport());
 
     var datascource = {
       'name': 'Lao Lao',
-      'title': 'general manager',
+      'title': 'President Office',
       'relationship': '001',
+      'position': [-87.6297980, 41.8781140],
       'children': [
-        { 'name': 'Bo Miao', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Su Miao', 'title': 'department manager', 'relationship': '111',
-          'children': [
-            { 'name': 'Tie Hua', 'title': 'senior engineer', 'relationship': '110' },
-            { 'name': 'Hei Hei', 'title': 'senior engineer', 'relationship': '111',
-              'children': [
-                { 'name': 'Pang Pang', 'title': 'engineer', 'relationship': '110' },
-                { 'name': 'Xiang Xiang', 'title': 'UE engineer', 'relationship': '110' }
-              ]
-            }
-          ]
-        },
-        { 'name': 'Yu Jie', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Yu Li', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Hong Miao', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Yu Wei', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Chun Miao', 'title': 'department manager', 'relationship': '110' },
-        { 'name': 'Yu Tie', 'title': 'department manager', 'relationship': '110' }
+        { 'name': 'Bo Miao', 'title': 'Administration  Dept.', 'relationship': '110', 'position': [-83.0457540, 42.3314270]},
+        { 'name': 'Su Miao', 'title': 'R & D Dept.', 'relationship': '110', 'position': [-81.6943610, 41.4993200]},
+        { 'name': 'Yu Jie', 'title': 'Product Dept.', 'relationship': '110', 'position': [-71.0588800, 42.3600820]},
+        { 'name': 'Yu Li', 'title': 'Legal Dept.', 'relationship': '110', 'position': [-74.0059410, 40.7127840]},
+        { 'name': 'Hong Miao', 'title': 'Finance Dept.', 'relationship': '110', 'position': [-80.8431270, 35.2270870]},
+        { 'name': 'Yu Wei', 'title': 'Security Dept.', 'relationship': '110', 'position': [-81.6556510, 30.3321840]},
+        { 'name': 'Chun Miao', 'title': 'HR Dept. ', 'relationship': '110', 'position': [-81.3792360, 28.5383350]},
+        { 'name': 'Yu Tie', 'title': 'Marketing Dept.', 'relationship': '110', 'position': [-80.1917900, 25.7616800] }
       ]
     };
 
     $('#chart-container').orgchart({
       'data' : datascource,
-      'depth': 2,
       'nodeTitle': 'name',
-      'nodeContent': 'title'
+      'nodeContent': 'title',
+      'createNode': function($node, data) {
+        $node.on('click', function() {
+          var view = map.getView();
+          var duration = 2000;
+          var start = +new Date();
+          var pan = ol.animation.pan({
+            duration: duration,
+            source:  view.getCenter(),
+            start: start
+          });
+          var bounce = ol.animation.bounce({
+            duration: duration,
+            resolution: 4 * view.getResolution(),
+            start: start
+          });
+          map.beforeRender(pan, bounce);
+          view.setCenter(ol.proj.transform(data.position, 'EPSG:4326', 'EPSG:3857'));
+        });
+      }
     });
 
   });
