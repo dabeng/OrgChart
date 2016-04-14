b191c70f1b6b2dab5c5a9c3b42c957d54a746093
dabeng <dabeng413@gmail.com>
2016-04-14 10:10:23 +0800

    allows users to create orgchart based on <ul>

--- ul-datasource/scripts.js
+++ ul-datasource/scripts.js
@@ -4,40 +4,10 @@
 
   $(function() {
 
- var extractDatasource = function($li, data, total, count, callback) {
-    if ($.isEmptyObject(data)) {
-      data.name = $li.contents().eq(0).text().trim();
-      data.relationship= '001';
-    } else {
-      // if (data.children) {
-        data.children.push({ 'name': $li.contents().eq(0).text().trim(),
-          'relationship': '1' +( $li.siblings().length ? 1: 0) + ($li.children().length ? 1 : 0) });
-      // } else {
-      //   data.children = [{ 'name': $li.contents().eq(0).text().trim(),
-      //     'relationship': '1' + ($li.siblings().length ? 1: 0) + ($li.children().length ? 1 : 0) }];
-      // }
-    }
-    count++;
-    if (count === total) {
-      callback();
-    }
-    if ($li.children().length) {
-      $li.children().children().each(function(index, item) {
-        data.children = [];
-        // if ($(item).children().length) {
-        extractDatasource($(item), data, total, count+index, callback);
-      });
-    }
-  }
-var conversed = {};
-extractDatasource($('#ul-data').children(), conversed, $('#ul-data').find('li').length, 0, function() {
     $('#chart-container').orgchart({
-      'data' : conversed,
+      'data' : $('#ul-data'),
       'nodeTitle': 'name'
     });
-});
-
-
 
   });
 
