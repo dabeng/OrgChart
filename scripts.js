e2cd72b90d505d211dc78b412d10812e5a7cc93e
dabeng <dabeng413@gmail.com>
2016-04-15 20:28:55 +0800

    clean up code

--- scripts.js
+++
@@ -1,9 +0,0 @@
-  function getHierarchy($orgchart) {
-    var $temp = $orgchart.find('tr:first');
-    var subObj = { 'id': $temp.find('.node')[0].id };
-    $temp.siblings(':last').children().each(function() {
-      if (!subObj.children) { subObj.children = []; }
-      subObj.children.push(getHierarchy($(this)));
-    });
-    return subObj;
-  }
