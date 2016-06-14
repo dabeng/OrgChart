a45f4a93b7b5454aaafa34886da335bb11c818e0
dabeng <dabeng413@gmail.com>
2016-06-14 16:41:24 +0800

    append the example of color-coded

--- color-coded/scripts.js
+++ color-coded/scripts.js
@@ -8,31 +8,31 @@
       'name': 'Lao Lao',
       'title': 'general manager',
       'children': [
-        { 'name': 'Bo Miao', 'title': 'department manager',
+        { 'name': 'Bo Miao', 'title': 'department manager', 'className': 'middle-level',
           'children': [
-            { 'name': 'Li Jing', 'title': 'senior engineer' },
-            { 'name': 'Li Xin', 'title': 'senior engineer',
+            { 'name': 'Li Jing', 'title': 'senior engineer', 'className': 'product-dept' },
+            { 'name': 'Li Xin', 'title': 'senior engineer', 'className': 'product-dept',
               'children': [
-                { 'name': 'To To', 'title': 'engineer' },
-                { 'name': 'Fei Fei', 'title': 'engineer' },
-                { 'name': 'Xuan Xuan', 'title': 'engineer' }
+                { 'name': 'To To', 'title': 'engineer', 'className': 'pipeline1' },
+                { 'name': 'Fei Fei', 'title': 'engineer', 'className': 'pipeline1' },
+                { 'name': 'Xuan Xuan', 'title': 'engineer', 'className': 'pipeline1' }
               ]
             }
           ]
         },
-        { 'name': 'Su Miao', 'title': 'department manager',
+        { 'name': 'Su Miao', 'title': 'department manager', 'className': 'middle-level',
           'children': [
-            { 'name': 'Pang Pang', 'title': 'senior engineer' },
-            { 'name': 'Hei Hei', 'title': 'senior engineer',
+            { 'name': 'Pang Pang', 'title': 'senior engineer', 'className': 'rd-dept' },
+            { 'name': 'Hei Hei', 'title': 'senior engineer', 'className': 'rd-dept',
               'children': [
-                { 'name': 'Xiang Xiang', 'title': 'UE engineer' },
-                { 'name': 'Dan Dan', 'title': 'engineer' },
-                { 'name': 'Zai Zai', 'title': 'engineer' }
+                { 'name': 'Xiang Xiang', 'title': 'UE engineer', 'className': 'frontend1' },
+                { 'name': 'Dan Dan', 'title': 'engineer', 'className': 'frontend1' },
+                { 'name': 'Zai Zai', 'title': 'engineer', 'className': 'frontend1' }
               ]
             }
           ]
         },
-        { 'name': 'Hong Miao', 'title': 'department manager' }
+        { 'name': 'Hong Miao', 'title': 'department manager', 'className': 'middle-level' }
       ]
     };
 
