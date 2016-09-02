756076d31d5520372fcc7425556b9c69bab28377
Trevor Frizzell <tfrizzell@gmail.com>
2016-09-02 15:01:30 -0300

    Fixed issues calling addChildren on root node

--- dist/js/jquery.orgchart.js
+++ dist/js/jquery.orgchart.js
@@ -487,7 +487,8 @@
   }
 
   function repaint(node) {
-    node.style.offsetWidth = node.offsetWidth;
+	if (node)
+      node.style.offsetWidth = node.offsetWidth;
   }
 
   // create node
@@ -812,6 +813,7 @@
   }
   // exposed method
   function addChildren($node, data, opts) {
+    var opts = opts || $node.closest('.orgchart').data('options');
     var count = 0;
     buildChildNode.call($node.closest('.orgchart').parent(), $node.closest('table'), data, opts, function() {
       if (++count === data.children.length) {
