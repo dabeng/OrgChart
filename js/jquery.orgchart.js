04e2f5069630c788a76fb3278eddf56f00f80012
dabeng <dabeng413@gmail.com>
2016-09-22 14:42:59 +0800

    Mid: enable option zoom on mobile device

--- js/jquery.orgchart.js
+++ js/jquery.orgchart.js
@@ -238,7 +238,7 @@
           var dist = Math.sqrt((e.touches[0].clientX - e.touches[1].clientX) * (e.touches[0].clientX - e.touches[1].clientX) +
             (e.touches[0].clientY - e.touches[1].clientY) * (e.touches[0].clientY - e.touches[1].clientY)).toFixed(2);
           var newScale = (dist/($chart.data('pinchDist')*10)).toFixed(6);
-          // alert(dist+','+$chart.data('pinchDist')+','+newScale);
+          alert(newScale);
           setChartScale($chart, newScale);
         }
       })
