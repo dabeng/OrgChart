b9c9ed78def005853cbe9b05e5382aa894461fab
dabeng <dabeng413@gmail.com>
2016-10-13 14:23:12 +0800

    expose more internal methods

--- js/jquery.orgchart.js
+++ js/jquery.orgchart.js
@@ -54,6 +54,16 @@
         return removeNodes.apply(this, Array.prototype.splice.call(arguments, 1));
       case 'getHierarchy':
         return getHierarchy.apply(this, Array.prototype.splice.call(arguments, 1));
+      case 'hideDescendants':
+        return hideDescendants.apply(this, Array.prototype.splice.call(arguments, 1));
+      case 'showDescendants':
+        return showDescendants.apply(this, Array.prototype.splice.call(arguments, 1));
+      case 'hideSiblings':
+        return hideSiblings.apply(this, Array.prototype.splice.call(arguments, 1));
+      case 'showSiblings':
+        return showSiblings.apply(this, Array.prototype.splice.call(arguments, 1));
+      case 'getNodeState':
+        return getNodeState.apply(this, Array.prototype.splice.call(arguments, 1));
       default: // initiation time
         var opts = $.extend(defaultOptions, options);
     }
@@ -322,11 +332,11 @@
     }
     if ($target.length) {
       if ($target.is(':visible')) {
-        return {"exist": true, "visible": true};
+        return { 'exist': true, 'visible': true, 'nodes': $target };
       }
-      return {"exist": true, "visible": false};
+      return { 'exist': true, 'visible': false, 'nodes': $target };
     }
-    return {"exist": false, "visible": false};
+    return { 'exist': false, 'visible': false, 'nodes': $target };
   }
 
   // recursively hide the ancestor node and sibling nodes of the specified node
@@ -559,9 +569,12 @@
 
   // create node
   function createNode(nodeData, level, opts) {
+    $.each(nodeData.children, function (index, child) {
+      child.parentId = nodeData.id;
+    })
     var dtd = $.Deferred();
     // construct the content of node
-    var $nodeDiv = $('<div' + (opts.draggable ? ' draggable="true"' : '') + (nodeData[opts.nodeId] ? ' id="' + nodeData[opts.nodeId] + '"' : '') + '>')
+    var $nodeDiv = $('<div' + (opts.draggable ? ' draggable="true"' : '') + (nodeData[opts.nodeId] ? ' id="' + nodeData[opts.nodeId] + '"' : '') + (nodeData.parentId ? ' data-parent="' + nodeData.parentId + '"' : '') + '>')
       .addClass('node ' + (nodeData.className || '') +  (level >= opts.depth ? ' slide-up' : ''))
       .append('<div class="title">' + nodeData[opts.nodeTitle] + '</div>')
       .append(typeof opts.nodeContent !== 'undefined' ? '<div class="content">' + (nodeData[opts.nodeContent] || '') + '</div>' : '');
