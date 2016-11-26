423dc96584da29631dee86adc6403d8623b21d58
dabeng <dabeng413@gmail.com>
2016-11-26 13:57:21 +0800

    refresh js version to 1.2.9

--- js/jquery.orgchart.js
+++ js/jquery.orgchart.js
@@ -584,7 +584,7 @@
   function createNode(nodeData, level, opts) {
     $.each(nodeData.children, function (index, child) {
       child.parentId = nodeData.id;
-    })
+    });
     var dtd = $.Deferred();
     // construct the content of node
     var $nodeDiv = $('<div' + (opts.draggable ? ' draggable="true"' : '') + (nodeData[opts.nodeId] ? ' id="' + nodeData[opts.nodeId] + '"' : '') + (nodeData.parentId ? ' data-parent="' + nodeData.parentId + '"' : '') + '>')
@@ -595,7 +595,8 @@
     var flags = nodeData.relationship || '';
     if (opts.verticalDepth && (level + 2) > opts.verticalDepth) {
       if ((level + 1) >= opts.verticalDepth && Number(flags.substr(2,1))) {
-        $nodeDiv.append('<i class="toggleBtn fa fa-minus-square"></i>');
+        var icon = level + 1  >= opts.depth ? 'plus' : 'minus';
+        $nodeDiv.append('<i class="toggleBtn fa fa-' + icon + '-square"></i>');
       }
     } else {
       if (Number(flags.substr(0,1))) {
@@ -736,7 +737,8 @@
           $descendants.removeClass('slide');
           // $descWrapper.addClass('hidden');
           $descendants.closest('ul').addClass('hidden');
-        }).find('.toggleBtn').removeClass('fa-minus-square').addClass('fa-plus-square');
+        });
+        $descendants.find('.toggleBtn').removeClass('fa-minus-square').addClass('fa-plus-square');
       }
     });
 
@@ -976,8 +978,11 @@
       var $nodeLayer;
       if (isVerticalLayer) {
         $nodeLayer = $('<ul>');
+        if (isHidden) {
+          $nodeLayer.addClass(isHidden);
+        }
         if (level + 2 === opts.verticalDepth) {
-          $nodeWrapper.append('<tr class="verticalNodes"><td></td></tr>')
+          $nodeWrapper.append('<tr class="verticalNodes' + isHidden + '"><td></td></tr>')
             .find('.verticalNodes').children().append($nodeLayer);
         } else {
           $nodeWrapper.append($nodeLayer);
