ea6d05d6680c38d3a2a39d1515511a6a8fc84ae9
dabeng <dabeng413@gmail.com>
2017-09-06 17:37:21 +0800

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,71 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
+// const gulp = require('gulp');
+// const mocha = require('gulp-mocha');
 
-gulp.task('default', () => {
-  gulp.src(['test/**/*-tests.js'], {read: false})
-    .pipe(mocha({reporter: 'spec'}));
+// gulp.task('default', () => {
+//   gulp.src(['test/**/*-tests.js'], {read: false})
+//     .pipe(mocha({reporter: 'spec'}));
+// });
+
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+var browserSync = require('browser-sync').create();
+var paths = {
+  src: 'src/**/*',
+  srcHTML: 'src/**/*.html',
+  srcCSS: 'src/**/*.css',
+  srcJS: 'src/**/*.js',
+  tmp: 'tmp',
+  tmpHTML: 'tmp/**/*.html',
+  tmpCSSFolder: 'tmp/css',
+  tmpCSS: 'tmp/**/*.css',
+  tmpJSFolder: 'tmp/js',
+  tmpJS: 'tmp/**/*.js',
+  dist: 'dist',
+  distIndex: 'dist/index.html',
+  distCSS: 'dist/**/*.css',
+  distJS: 'dist/**/*.js'
+};
+
+gulp.task('default', function () {
+  console.log('Hello World!');
+});
+
+gulp.task('html', function () {
+  return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('css', function () {
+  return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('vendorCSS', function () {
+  return gulp.src(['node_modules/font-awesome/css/font-awesome.min.css'])
+    .pipe(gulp.dest(paths.tmpCSSFolder));
+});
+
+gulp.task('js', function () {
+  return gulp.src(paths.srcJS).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('vendorJS', function () {
+  return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/jquery-mockjax/dist/jquery.mockjax.min.js'])
+    .pipe(gulp.dest(paths.tmpJSFolder));
+});
+
+gulp.task('copy', ['html', 'css', 'js', 'vendorCSS', 'vendorJS']);
+
+gulp.task('inject', ['copy'], function () {
+  return gulp.src(paths.tmpHTML)
+    .pipe(inject(gulp.src(paths.tmpCSS), { relative:true }))
+    .pipe(inject(gulp.src(paths.tmpJS), { relative:true }))
+    .pipe(gulp.dest(paths.tmp));
+});
+
+// Static server
+gulp.task('serve', ['inject'], function() {
+    browserSync.init({
+        server: {
+            baseDir: paths.tmp
+        }
+    });
 });
