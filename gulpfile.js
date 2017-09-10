f4623a074f7d716d64b5b417e7c7d641daa5657e
dabeng <dabeng413@gmail.com>
2017-09-10 21:49:41 +0800

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require('gulp');
-var inject = require('gulp-inject');
 var browserSync = require('browser-sync').create();
 var mocha = require('gulp-mocha');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
 var del = require('del');
 var eslint = require('gulp-eslint');
+var merge = require('merge-stream');
 var paths = {
-  src: 'src/**/*',
+  src: 'src',
+  srcFiles: 'src/**/*',
   srcHTML: 'src/**/*.html',
   srcCSS: 'src/**/*.css',
   srcJS: 'src/**/*.js',
   demo: 'demo',
+  demoFiles: 'demo/**/*',
   demoHTML: 'demo/**/*.html',
   demoCSSFolder: 'demo/css',
   demoCSS: 'demo/**/*.css',
@@ -28,60 +30,8 @@ gulp.task('default', function () {
   console.log('Hello World!');
 });
 
-gulp.task('html', function () {
-  return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.demo));
-});
-
-gulp.task('css', function () {
-  return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.demo));
-});
-
-gulp.task('vendorCSS', function () {
-  return gulp.src(['node_modules/font-awesome/css/font-awesome.min.css'])
-    .pipe(gulp.dest(paths.demoCSSFolder));
-});
-
-gulp.task('js', function () {
-  return gulp.src(paths.srcJS).pipe(gulp.dest(paths.demo));
-});
-
-gulp.task('vendorJS', function () {
-  return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/jquery-mockjax/dist/jquery.mockjax.min.js'])
-    .pipe(gulp.dest(paths.demoJSFolder));
-});
-
-gulp.task('vendorAssets', function () {
-  return gulp.src(['node_modules/font-awesome/fonts/**/*'])
-    .pipe(gulp.dest(paths.demo + '/fonts'));
-});
-
-gulp.task('copy', ['html', 'css', 'js', 'vendorCSS', 'vendorJS', 'vendorAssets']);
-
-gulp.task('inject', ['copy'], function () {
-  return gulp.src(paths.demoHTML)
-    .pipe(inject(gulp.src([paths.demoCSS, paths.demoJS], {read: false}), { relative:true }))
-    .pipe(gulp.dest(paths.demo));
-});
-
-gulp.task('serve', ['inject'], function() {
-  browserSync.init({
-    server: {
-      baseDir: paths.demo
-    }
-  });
-});
-
-gulp.task('reload', ['inject'], function (done) {
-  browserSync.reload();
-  done();
-});
-
-gulp.task('watch', ['serve'], function () {
-  gulp.watch(paths.src, ['reload']);
-});
-
 gulp.task('cleanJS', function() {
-  return del(paths.distJSFolder);
+  del([paths.demoJSFolder + '/*orgchart*', paths.distJSFolder]);
 });
 
 gulp.task('eslint', function () {
@@ -95,11 +45,50 @@ gulp.task('test', ['eslint'], function () {
     .pipe(mocha({reporter: 'spec'}));
 });
 
-gulp.task('distJS', ['cleanJS', 'test'], function () {
+gulp.task('js', ['cleanJS', 'test'], function () {
   return gulp.src(paths.srcJS)
     .pipe(uglify())
     .pipe(rename('jquery.orgchart.min.js'))
+    .pipe(gulp.dest(paths.demoJSFolder))
     .pipe(gulp.dest(paths.distJSFolder));
 });
 
-gulp.task('build', ['distJS']);
+gulp.task('css', function () {
+  return gulp.src(paths.srcCSS)
+    .pipe(gulp.dest(paths.demo))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('vendorAssets', function() {
+  var fontawesomeCSS = gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest(paths.demoCSSFolder));
+
+  var fontawesomeFonts = gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest(paths.demo + '/fonts'));
+
+  var vendorJS = gulp.src([
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/jquery-mockjax/dist/jquery.mockjax.min.js',
+      'node_modules/html2canvas/dist/html2canvas.min.js'
+    ])
+    .pipe(gulp.dest(paths.demoJSFolder));
+
+  return merge(fontawesomeCSS, fontawesomeFonts, vendorJS);
+});
+
+gulp.task('build', ['js', 'css', 'vendorAssets']);
+
+gulp.task('reload', function (done) {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('serve', ['build'], function() {
+  browserSync.init({
+    server: {
+      baseDir: paths.demo
+    }
+  });
+  gulp.watch(paths.srcFiles, ['build']);
+  gulp.watch(paths.demoFiles, ['reload']);
+});
