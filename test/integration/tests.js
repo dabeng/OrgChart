0f8338bcaea3560c0d48f23a7529036163431b21
dabeng <dabeng413@gmail.com>
2018-01-08 20:42:33 +0800

    WIP: add integration tests

--- test/integration/tests.js
+++ test/integration/tests.js
@@ -1,7 +1,7 @@
 var chai = require("chai");
 var sinon = require("sinon");
 var sinonChai = require("sinon-chai");
-chai.should();
+var should = chai.should();
 chai.use(sinonChai);
 var jsdom = require("jsdom");
 var { JSDOM } = jsdom;
@@ -101,17 +101,57 @@ describe('orgchart -- integration tests', function () {
     $container.empty();
   });
 
-  it("Adding new root node", function () {
+  it('addParent()', function () {
     oc.addParent($laolao, { 'name': 'Lao Ye', 'id': 'n0' });
     $laolao.closest('.nodes').prevAll().should.lengthOf(3);
     oc.$chart.find('.node:first').should.deep.equal($('#n0'));
   });
 
-  it("when one node's parent is hidden, its sibling branches are hidden too", function () {
-    oc.hideParent($hongmiao);
-    $bomiao.is('.slide-right').should.be.true;
-    $sumiao.is('.slide-right').should.be.true;
-    $sumiao.find('.slide-right').should.lengthOf($sumiao.find('.node').length);
+  describe('addChildren()', function () {
+    it('Add child nodes to the leaf node', function () {
+      oc.addChildren($bomiao, { 'children': [{'name': 'Li Xin', 'id': 'n11' }] });
+      $bomiao.closest('tr').siblings().should.lengthOf(3);
+      $bomiao.closest('tr').siblings('.nodes').find('.node').attr('id').should.equal('n11');
+    });
+
+    it('Add child nodes to the un-leaf node', function () {
+      oc.addChildren($sumiao, { 'children': [{'name': 'Li Xin', 'id': 'n11' }] });
+      $sumiao.closest('tr').siblings('.nodes').children().should.lengthOf(4);
+      $sumiao.closest('tr').siblings('.nodes').children(':last').find('.node').attr('id').should.equal('n11');
+    });
+  });
+
+  describe('addSiblings()', function () {
+    it('Just add sibling nodes', function () {
+      oc.addSiblings($sumiao, { 'siblings': [{'name': 'Li Xin', 'id': 'n11' }] });
+      $laolao.closest('tr').siblings('.nodes').children().should.lengthOf(4);
+      $laolao.closest('tr').siblings('.nodes').children(':last').find('.node').attr('id').should.equal('n11');
+    });
+
+    it('Add sibling nodes as well as parent node', function () {
+      oc.addSiblings($laolao, { 'name': 'Lao Ye', 'id': 'n0', 'children': [{'name': 'Li Xin', 'id': 'n11' }] });
+      $laolao.closest('.nodes').prevAll().should.lengthOf(3);
+      oc.$chart.find('.node:first').should.deep.equal($('#n0'));
+      $laolao.closest('table').parent().siblings().should.lengthOf(1);
+      $laolao.closest('table').parent().siblings().find('.node').attr('id').should.equal('n11');
+    });
+  });
+
+  describe('removeNodes()', function () {
+    it('Remove leaf node', function () {
+      oc.removeNodes($dandan);
+      $tiehua.closest('tr').siblings().should.lengthOf(0);
+    });
+    it('Remove parent node', function () {
+      oc.removeNodes($tiehua);
+      $sumiao.closest('tr').siblings('.nodes').children().should.lengthOf(2);
+      $('#n5').should.lengthOf(0);
+      $('#n8').should.lengthOf(0);
+    });
+    it('Remove parent node', function () {
+      oc.removeNodes($laolao);
+      oc.$chartContainer.is(':empty').should.be.true;
+    });
   });
 
 });
