73ce22592fdfe30f426b34faa567ec1ec5602024
dabeng <dabeng413@gmail.com>
2019-10-07 09:00:01 +0800

    feat: update html2canvas

--- src/js/jquery.orgchart.js
+++ src/js/jquery.orgchart.js
@@ -1046,7 +1046,7 @@
       this.touchMoved = true;
       var $touching = $(document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY));
       var $touchingNode = $touching.closest('div.node');
- 
+
       if ($touchingNode.length > 0) {
         var touchingNodeElement = $touchingNode[0];
         // TODO: simulate the dragover visualisation
@@ -1097,20 +1097,20 @@
       var simulatedEvent = document.createEvent('MouseEvents');
       simulatedEvent.initMouseEvent(
         simulatedType,    // type
-        true,             // bubbles                    
-        true,             // cancelable                 
-        window,           // view                       
-        1,                // detail                     
-        touch.screenX,    // screenX                    
-        touch.screenY,    // screenY                    
-        touch.clientX,    // clientX                    
-        touch.clientY,    // clientY                    
-        false,            // ctrlKey                    
-        false,            // altKey                     
-        false,            // shiftKey                   
-        false,            // metaKey                    
-        0,                // button                     
-        null              // relatedTarget              
+        true,             // bubbles
+        true,             // cancelable
+        window,           // view
+        1,                // detail
+        touch.screenX,    // screenX
+        touch.screenY,    // screenY
+        touch.clientX,    // clientX
+        touch.clientY,    // clientY
+        false,            // ctrlKey
+        false,            // altKey
+        false,            // shiftKey
+        false,            // metaKey
+        0,                // button
+        null              // relatedTarget
       );
       // Dispatch the simulated event to the target element
       event.target.dispatchEvent(simulatedEvent);
@@ -1353,6 +1353,49 @@
       }
     },
     //
+    exportPDF: function(canvas, exportFilename){
+      var doc = {};
+      var docWidth = Math.floor(canvas.width);
+      var docHeight = Math.floor(canvas.height);
+
+      if (docWidth > docHeight) {
+        doc = new jsPDF({
+          orientation: 'landscape',
+          unit: 'mm',
+          format: [docWidth, docHeight]
+        });
+      } else {
+        doc = new jsPDF({
+          orientation: 'portrait',
+          unit: 'mm',
+          format: [docHeight, docWidth]
+        });
+      }
+      doc.addImage(canvas.toDataURL(), 'png', 0, 0);
+      doc.save(exportFilename + '.pdf');
+    },
+    //
+    exportPNG: function(canvas, exportFilename){
+      var that = this;
+      var isWebkit = 'WebkitAppearance' in document.documentElement.style;
+      var isFf = !!window.sidebar;
+      var isEdge = navigator.appName === 'Microsoft Internet Explorer' || (navigator.appName === "Netscape" && navigator.appVersion.indexOf('Edge') > -1);
+      var $chartContainer = this.$chartContainer;
+
+      if ((!isWebkit && !isFf) || isEdge) {
+        window.navigator.msSaveBlob(canvas.msToBlob(), exportFilename + '.png');
+      } else {
+        var selector = '.oc-download-btn' + (that.options.chartClass !== '' ? '.' + that.options.chartClass : '');
+
+        if (!$chartContainer.find(selector).length) {
+          $chartContainer.append('<a class="oc-download-btn' + (that.options.chartClass !== '' ? ' ' + that.options.chartClass : '') + '"'
+                                 + ' download="' + exportFilename + '.png"></a>');
+        }
+
+        $chartContainer.find(selector).attr('href', canvas.toDataURL())[0].click();
+      }
+    },
+    //
     export: function (exportFilename, exportFileextension) {
       var that = this;
       exportFilename = (typeof exportFilename !== 'undefined') ?  exportFilename : this.options.exportFilename;
@@ -1375,39 +1418,17 @@
         'onclone': function (cloneDoc) {
           $(cloneDoc).find('.canvasContainer').css('overflow', 'visible')
             .find('.orgchart:not(".hidden"):first').css('transform', '');
-        },
-        'onrendered': function (canvas) {
-          $chartContainer.find('.mask').addClass('hidden');
-          if (exportFileextension.toLowerCase() === 'pdf') {
-            var doc = {};
-            var docWidth = Math.floor(canvas.width * 0.2646);
-            var docHeight = Math.floor(canvas.height * 0.2646);
-            if (docWidth > docHeight) {
-              doc = new jsPDF('l', 'mm', [docWidth, docHeight]);
-            } else {
-              doc = new jsPDF('p', 'mm', [docHeight, docWidth]);
-            }
-            doc.addImage(canvas.toDataURL(), 'png', 0, 0);
-            doc.save(exportFilename + '.pdf');
-          } else {
-            var isWebkit = 'WebkitAppearance' in document.documentElement.style;
-            var isFf = !!window.sidebar;
-            var isEdge = navigator.appName === 'Microsoft Internet Explorer' || (navigator.appName === "Netscape" && navigator.appVersion.indexOf('Edge') > -1);
-
-            if ((!isWebkit && !isFf) || isEdge) {
-              window.navigator.msSaveBlob(canvas.msToBlob(), exportFilename + '.png');
-            } else {
-              var selector = '.oc-download-btn' + (that.options.chartClass !== '' ? '.' + that.options.chartClass : '');
-              if (!$chartContainer.find(selector).length) {
-                $chartContainer.append('<a class="oc-download-btn' + (that.options.chartClass !== '' ? ' ' + that.options.chartClass : '') + '"'
-                  + ' download="' + exportFilename + '.png"></a>');
-              }
-              $chartContainer.find(selector).attr('href', canvas.toDataURL())[0].click();
-            }
-          }
         }
       })
-      .then(function () {
+      .then(function (canvas) {
+        $chartContainer.find('.mask').addClass('hidden');
+
+        if (exportFileextension.toLowerCase() === 'pdf') {
+          that.exportPDF(canvas, exportFilename);
+        } else {
+          that.exportPNG(canvas, exportFilename);
+        }
+
         $chartContainer.removeClass('canvasContainer');
       }, function () {
         $chartContainer.removeClass('canvasContainer');
