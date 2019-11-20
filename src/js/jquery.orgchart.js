34ffa97b29a4d553bc0ba552b55195a4ca902ecd
lovecyc <35739948+lovecyc@users.noreply.github.com>
2019-11-20 17:18:47 +0800

    fix parentID(data-parent)
    use data[opts.nodeId] to init data-parent for every children,
    instead of data.id

--- src/js/jquery.orgchart.js
+++ src/js/jquery.orgchart.js
@@ -1216,9 +1216,9 @@
       var that = this;
       var opts = this.options;
       var level = data.level;
-      if (data.children) {
+      if (data.children && data[opts.nodeId]) {
         $.each(data.children, function (index, child) {
-          child.parentId = data.id;
+          child.parentId = data[opts.nodeId]
         });
       }
       // construct the content of node
