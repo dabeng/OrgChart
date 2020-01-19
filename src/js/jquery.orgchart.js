571fc97269493861867efe6b3ef3edf7f1fd8cc2
dabeng <dabeng413@gmail.com>
2020-01-19 11:11:58 +0800

    WIP: refactor all the demos

--- src/js/jquery.orgchart.js
+++ src/js/jquery.orgchart.js
@@ -29,7 +29,7 @@
       'exportButton': false,
       'exportFilename': 'OrgChart',
       'exportFileextension': 'png',
-      'parentNodeSymbol': 'oc-leader',
+      'parentNodeSymbol': 'oci-leader',
       'draggable': false,
       'direction': 't2b',
       'pan': false,
@@ -69,7 +69,7 @@
           this.buildHierarchy($chart, this.options.ajaxURL ? data : this.attachRel(data, '00'));
         }
       } else {
-        $chart.append('<i class="oc oc-spinner spinner"></i>');
+        $chart.append('<i class="oci oci-spinner spinner"></i>');
         $.ajax({
           'url': data,
           'dataType': 'json'
@@ -87,7 +87,7 @@
       $chartContainer.append($chart);
 
       // append the export button
-      if (this.options.exportButton && !$chartContainer.find('.oc-export-btn').length) {
+      if (this.options.exportButton && !$chartContainer.find('.oci-export-btn').length) {
         this.attachExportButton();
       }
 
@@ -126,7 +126,7 @@
     attachExportButton: function () {
       var that = this;
       var $exportBtn = $('<button>', {
-        'class': 'oc-export-btn' + (this.options.chartClass !== '' ? ' ' + this.options.chartClass : ''),
+        'class': 'oci-export-btn' + (this.options.chartClass !== '' ? ' ' + this.options.chartClass : ''),
         'text': 'Export',
         'click': function(e) {
           e.preventDefault();
@@ -610,7 +610,7 @@
       event.data.visibleNodes.removeClass('sliding');
       if (this.isInAction($node)) {
         this.switchHorizontalArrow($node);
-        $node.children('.topEdge').removeClass('oc-chevron-up').addClass('oc-chevron-down');
+        $node.children('.topEdge').removeClass('oci-chevron-up').addClass('oci-chevron-down');
       }
     },
     //
@@ -659,10 +659,10 @@
       }
 
       $edge.addClass('hidden');
-      $edge.parent().append('<i class="oc oc-spinner spinner"></i>')
+      $edge.parent().append('<i class="oci oci-spinner spinner"></i>')
         .children().not('.spinner').css('opacity', 0.2);
       $chart.data('inAjax', true);
-      $('.oc-export-btn' + (this.options.chartClass !== '' ? '.' + this.options.chartClass : '')).prop('disabled', true);
+      $('.oci-export-btn' + (this.options.chartClass !== '' ? '.' + this.options.chartClass : '')).prop('disabled', true);
       return true;
     },
     // terminate loading status for requesting new nodes
@@ -672,15 +672,15 @@
       $node.find('.spinner').remove();
       $node.children().removeAttr('style');
       this.$chart.data('inAjax', false);
-      $('.oc-export-btn' + (this.options.chartClass !== '' ? '.' + this.options.chartClass : '')).prop('disabled', false);
+      $('.oci-export-btn' + (this.options.chartClass !== '' ? '.' + this.options.chartClass : '')).prop('disabled', false);
     },
     // whether the cursor is hovering over the node
     isInAction: function ($node) {
-      return $node.children('.edge').attr('class').indexOf('oc-') > -1 ? true : false;
+      return $node.children('.edge').attr('class').indexOf('oci-') > -1 ? true : false;
     },
     //
     switchVerticalArrow: function ($arrow) {
-      $arrow.toggleClass('oc-chevron-up').toggleClass('oc-chevron-down');
+      $arrow.toggleClass('oci-chevron-up').toggleClass('oci-chevron-down');
     },
     //
     switchHorizontalArrow: function ($node) {
@@ -689,24 +689,24 @@
         var $prevSib = $node.closest('table').parent().prev();
         if ($prevSib.length) {
           if ($prevSib.is('.hidden')) {
-            $node.children('.leftEdge').addClass('oc-chevron-left').removeClass('oc-chevron-right');
+            $node.children('.leftEdge').addClass('oci-chevron-left').removeClass('oci-chevron-right');
           } else {
-            $node.children('.leftEdge').addClass('oc-chevron-right').removeClass('oc-chevron-left');
+            $node.children('.leftEdge').addClass('oci-chevron-right').removeClass('oci-chevron-left');
           }
         }
         var $nextSib = $node.closest('table').parent().next();
         if ($nextSib.length) {
           if ($nextSib.is('.hidden')) {
-            $node.children('.rightEdge').addClass('oc-chevron-right').removeClass('oc-chevron-left');
+            $node.children('.rightEdge').addClass('oci-chevron-right').removeClass('oci-chevron-left');
           } else {
-            $node.children('.rightEdge').addClass('oc-chevron-left').removeClass('oc-chevron-right');
+            $node.children('.rightEdge').addClass('oci-chevron-left').removeClass('oci-chevron-right');
           }
         }
       } else {
         var $sibs = $node.closest('table').parent().siblings();
         var sibsVisible = $sibs.length ? !$sibs.is('.hidden') : false;
-        $node.children('.leftEdge').toggleClass('oc-chevron-right', sibsVisible).toggleClass('oc-chevron-left', !sibsVisible);
-        $node.children('.rightEdge').toggleClass('oc-chevron-left', sibsVisible).toggleClass('oc-chevron-right', !sibsVisible);
+        $node.children('.leftEdge').toggleClass('oci-chevron-right', sibsVisible).toggleClass('oci-chevron-left', !sibsVisible);
+        $node.children('.rightEdge').toggleClass('oci-chevron-left', sibsVisible).toggleClass('oci-chevron-right', !sibsVisible);
       }
     },
     //
@@ -725,17 +725,17 @@
       if (event.type === 'mouseenter') {
         if ($topEdge.length) {
           flag = this.getNodeState($node, 'parent').visible;
-          $topEdge.toggleClass('oc-chevron-up', !flag).toggleClass('oc-chevron-down', flag);
+          $topEdge.toggleClass('oci-chevron-up', !flag).toggleClass('oci-chevron-down', flag);
         }
         if ($bottomEdge.length) {
           flag = this.getNodeState($node, 'children').visible;
-          $bottomEdge.toggleClass('oc-chevron-down', !flag).toggleClass('oc-chevron-up', flag);
+          $bottomEdge.toggleClass('oci-chevron-down', !flag).toggleClass('oci-chevron-up', flag);
         }
         if ($leftEdge.length) {
           this.switchHorizontalArrow($node);
         }
       } else {
-        $node.children('.edge').removeClass('oc-chevron-up oc-chevron-down oc-chevron-right oc-chevron-left');
+        $node.children('.edge').removeClass('oci-chevron-up oci-chevron-down oci-chevron-right oci-chevron-left');
       }
     },
     //
@@ -887,14 +887,14 @@
       var $descendants = $descWrapper.find('.node');
       var $children = $descWrapper.children().children('.node');
       if ($children.is('.sliding')) { return; }
-      $toggleBtn.toggleClass('oc-plus-square oc-minus-square');
+      $toggleBtn.toggleClass('oci-plus-square oci-minus-square');
       if ($descendants.eq(0).is('.slide-up')) {
         $descWrapper.removeClass('hidden');
         this.repaint($children.get(0));
         $children.addClass('sliding').removeClass('slide-up').eq(0).one('transitionend', { 'vNodes': $children }, this.expandVNodesEnd);
       } else {
         $descendants.addClass('sliding slide-up').eq(0).one('transitionend', { 'vNodes': $descendants }, this.collapseVNodesEnd);
-        $descendants.find('.toggleBtn').removeClass('oc-minus-square').addClass('oc-plus-square');
+        $descendants.find('.toggleBtn').removeClass('oci-minus-square').addClass('oci-plus-square');
       }
     },
     //
@@ -1019,7 +1019,7 @@
       }
       // firstly, deal with the hierarchy of drop zone
       if (!$dropZone.closest('tr').siblings().length) { // if the drop zone is a leaf node
-        $dropZone.append('<i class="edge verticalEdge bottomEdge oc"></i>')
+        $dropZone.append('<i class="edge verticalEdge bottomEdge oci"></i>')
           .parent().attr('colspan', 2)
           .parent().after('<tr class="lines"><td colspan="2"><div class="downLine"></div></td></tr>'
           + '<tr class="lines"><td class="rightLine"></td><td class="leftLine"></td></tr>'
@@ -1027,7 +1027,7 @@
           .siblings(':last').append($dragged.find('.horizontalEdge').remove().end().closest('table').parent());
       } else {
         var dropColspan = parseInt($dropZone.parent().attr('colspan')) + 2;
-        var horizontalEdges = '<i class="edge horizontalEdge rightEdge oc"></i><i class="edge horizontalEdge leftEdge oc"></i>';
+        var horizontalEdges = '<i class="edge horizontalEdge rightEdge oci"></i><i class="edge horizontalEdge leftEdge oci"></i>';
         $dropZone.closest('tr').next().addBack().children().attr('colspan', dropColspan);
         if (!$dragged.find('.horizontalEdge').length) {
           $dragged.append(horizontalEdges);
@@ -1239,19 +1239,19 @@
       if (opts.verticalLevel && level >= opts.verticalLevel) {
         if ((level + 1) > opts.verticalLevel && Number(flags.substr(2,1))) {
           var icon = level + 1 > opts.visibleLevel ? 'plus' : 'minus';
-          $nodeDiv.append('<i class="toggleBtn oc oc-' + icon + '-square"></i>');
+          $nodeDiv.append('<i class="toggleBtn oci oci-' + icon + '-square"></i>');
         }
       } else {
         if (Number(flags.substr(0,1))) {
-          $nodeDiv.append('<i class="edge verticalEdge topEdge oc"></i>');
+          $nodeDiv.append('<i class="edge verticalEdge topEdge oci"></i>');
         }
         if(Number(flags.substr(1,1))) {
-          $nodeDiv.append('<i class="edge horizontalEdge rightEdge oc"></i>' +
-            '<i class="edge horizontalEdge leftEdge oc"></i>');
+          $nodeDiv.append('<i class="edge horizontalEdge rightEdge oci"></i>' +
+            '<i class="edge horizontalEdge leftEdge oci"></i>');
         }
         if(Number(flags.substr(2,1))) {
-          $nodeDiv.append('<i class="edge verticalEdge bottomEdge oc"></i>')
-            .children('.title').prepend('<i class="oc '+ opts.parentNodeSymbol + ' symbol"></i>');
+          $nodeDiv.append('<i class="edge verticalEdge bottomEdge oci"></i>')
+            .children('.title').prepend('<i class="oci '+ opts.parentNodeSymbol + ' symbol"></i>');
         }
       }
 
@@ -1346,10 +1346,10 @@
     addChildren: function ($node, data) {
       this.buildChildNode($node.closest('table'), data);
       if (!$node.children('.bottomEdge').length) {
-        $node.append('<i class="edge verticalEdge bottomEdge oc"></i>');
+        $node.append('<i class="edge verticalEdge bottomEdge oci"></i>');
       }
       if (!$node.find('.symbol').length) {
-        $node.children('.title').prepend('<i class="oc '+ this.options.parentNodeSymbol + ' symbol"></i>');
+        $node.children('.title').prepend('<i class="oci '+ this.options.parentNodeSymbol + ' symbol"></i>');
       }
       if (this.isInAction($node)) {
         this.switchVerticalArrow($node.children('.bottomEdge'));
@@ -1370,7 +1370,7 @@
     addParent: function ($currentRoot, data) {
       this.buildParentNode($currentRoot, data);
       if (!$currentRoot.children('.topEdge').length) {
-        $currentRoot.children('.title').after('<i class="edge verticalEdge topEdge oc"></i>');
+        $currentRoot.children('.title').after('<i class="edge verticalEdge topEdge oci"></i>');
       }
       if (this.isInAction($currentRoot)) {
         this.switchVerticalArrow($currentRoot.children('.topEdge'));
@@ -1413,11 +1413,11 @@
       this.buildSiblingNode($node.closest('table'), data);
       $node.closest('.nodes').data('siblingsLoaded', true);
       if (!$node.children('.leftEdge').length) {
-        $node.children('.topEdge').after('<i class="edge horizontalEdge rightEdge oc"></i><i class="edge horizontalEdge leftEdge oc"></i>');
+        $node.children('.topEdge').after('<i class="edge horizontalEdge rightEdge oci"></i><i class="edge horizontalEdge leftEdge oci"></i>');
       }
       if (this.isInAction($node)) {
         this.switchHorizontalArrow($node);
-        $node.children('.topEdge').removeClass('oc-chevron-up').addClass('oc-chevron-down');
+        $node.children('.topEdge').removeClass('oci-chevron-up').addClass('oci-chevron-down');
       }
     },
     //
@@ -1500,10 +1500,10 @@
       if ((!isWebkit && !isFf) || isEdge) {
         window.navigator.msSaveBlob(canvas.msToBlob(), exportFilename + '.png');
       } else {
-        var selector = '.oc-download-btn' + (that.options.chartClass !== '' ? '.' + that.options.chartClass : '');
+        var selector = '.oci-download-btn' + (that.options.chartClass !== '' ? '.' + that.options.chartClass : '');
 
         if (!$chartContainer.find(selector).length) {
-          $chartContainer.append('<a class="oc-download-btn' + (that.options.chartClass !== '' ? ' ' + that.options.chartClass : '') + '"'
+          $chartContainer.append('<a class="oci-download-btn' + (that.options.chartClass !== '' ? ' ' + that.options.chartClass : '') + '"'
                                  + ' download="' + exportFilename + '.png"></a>');
         }
 
@@ -1521,7 +1521,7 @@
       var $chartContainer = this.$chartContainer;
       var $mask = $chartContainer.find('.mask');
       if (!$mask.length) {
-        $chartContainer.append('<div class="mask"><i class="oc oc-spinner spinner"></i></div>');
+        $chartContainer.append('<div class="mask"><i class="oci oci-spinner spinner"></i></div>');
       } else {
         $mask.removeClass('hidden');
       }
